07947bd4616ba0ee506f7bb47c6c2ebb8f8fc975
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-09-26 09:26:13 +0300

    creation de la page home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  // Fermer le menu si on clique en dehors
+  @HostListener('document:click', ['$event'])
+  closeOnClickOutside(event: Event) {
+    const clickedInsideMenu = (event.target as HTMLElement).closest('.navbar-collapse');
+    const clickedToggler = (event.target as HTMLElement).closest('.navbar-toggler');
+    if (!clickedInsideMenu && !clickedToggler) {
+      this.closeMenu();
+    }
+  }
 }
